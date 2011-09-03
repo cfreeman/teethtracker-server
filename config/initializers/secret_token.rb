cd6241323d35d2fdd4c7e5a0f8bf8821b0f7dea2
Clinton <clinton.freeman@nicta.com.au>
2011-09-03 14:54:49 +1000

    Added initial sources.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluetracker::Application.config.secret_token = 'baaf1cdc74975fa1083e6a0f8c8f21aa0dbeda88ede59f3bfb0a26f0b9242fbe25f4ccf74a371ac9df61749deee209b7a4c8b939723c275266917f1eb59a4f72'
