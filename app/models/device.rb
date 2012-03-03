42c5b7e66301fcba21f9d9444bdba2bb88f72baf
Clinton <clinton.freeman@nicta.com.au>
2012-03-03 13:02:12 +1000

    returned local number rather than international.

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,16 @@
 class Device < ActiveRecord::Base
   validates :bluetooth_id, :uniqueness => true
 
-  #def device_number=(number)
+  def device_number= (number)
+    write_attribute(:device_number, @device.device_number = "+61" + number[1, number.length])
+  end
 
-  #end
+  def device_number
+    number =  read_attribute(:device_number)
+    "0" + number[3, number]
+  end
+
+  def device_international_number
+    read_attribute(:device_number)
+  end
 end
