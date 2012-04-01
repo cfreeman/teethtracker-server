a405526021814518c4233150addca0a49dd5ae4a
Clinton <clinton.freeman@nicta.com.au>
2012-04-01 14:28:49 +1000

    Disabled enum.

--- app/models/device_movement.rb
+++ app/models/device_movement.rb
@@ -1,5 +1,5 @@
 class DeviceMovement < ActiveRecord::Base
-  validates_inclusion_of :movement_type, :in => [:arrival, :departure]
+  #validates_inclusion_of :movement_type, :in => [:arrival, :departure]
 
   def self.current_devices(zone)
     DeviceMovement.where("node = ? and movement_type = ?", zone, "arrival").order("created_at DESC").select("DISTINCT(device_bluetooth_id), created_at")
