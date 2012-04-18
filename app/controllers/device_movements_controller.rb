5ff32bbd025706e2fe9d159687ba17e94265ec2d
Clinton <clinton.freeman@nicta.com.au>
2012-04-18 21:04:58 +1000

    Added pathway choice.

--- app/controllers/device_movements_controller.rb
+++ app/controllers/device_movements_controller.rb
@@ -109,9 +109,35 @@ class DeviceMovementsController < ApplicationController
     end
   end
 
-  def station6
+  def station6A
     @response = Twilio::TwiML::Response.new do |r|
-      r.Play 'http://teethtracker.heroku.com/call06.mp3'
+      r.Play 'http://teethtracker.heroku.com/call06A.mp3'
+      r.Dial :callerId => '+19138151163' do |d|
+        d.Client 'Jenny'
+      end
+    end
+
+    respond_to do |format|
+      format.all {render :xml => @response.text }
+    end
+  end
+
+  def station6B
+    @response = Twilio::TwiML::Response.new do |r|
+      r.Play 'http://teethtracker.heroku.com/call06B.mp3'
+      r.Dial :callerId => '+19138151163' do |d|
+        d.Client 'Jenny'
+      end
+    end
+
+    respond_to do |format|
+      format.all {render :xml => @response.text }
+    end
+  end
+
+  def station7
+    @response = Twilio::TwiML::Response.new do |r|
+      r.Play 'http://teethtracker.heroku.com/call07.mp3'
       r.Dial :callerId => '+19138151163' do |d|
         d.Client 'Jenny'
       end
